855dcb7807bc85e51f2beffbd66e926191792217
Cortuzz <oleg.cortuzz1@gmail.com>
2022-10-21 19:02:57 +0700

    🖼resource(general): Added design preferences
    - FontFamily: IBM Plex
    - Main design colors

--- app/src/main/java/com/example/mobiledevelopment/ui/theme/Color.kt
+++ app/src/main/java/com/example/mobiledevelopment/ui/theme/Color.kt
@@ -7,4 +7,8 @@ val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 
+val AccentColor = Color(0xFFEF3A01)
 val BackgroundColor = Color(0xFF150D0B)
+val OutlineColor = Color(0xFFB7B7B7)
+val TextColor = Color(0xFFD1D1D1)
+val SelectedTextColor = Color(0xFFE7E7E7)
--- app/src/main/java/com/example/mobiledevelopment/ui/theme/Type.kt
+++ app/src/main/java/com/example/mobiledevelopment/ui/theme/Type.kt
@@ -2,9 +2,12 @@ package com.example.mobiledevelopment.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.mobiledevelopment.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -26,3 +29,21 @@ val Typography = Typography(
     )
     */
 )
+
+
+val IBMPlex = FontFamily(
+        Font(R.font.ibmplexsans_bold, FontWeight.Bold, FontStyle.Normal),
+        Font(R.font.ibmplexsans_bolditalic, FontWeight.Bold, FontStyle.Italic),
+        Font(R.font.ibmplexsans_italic, FontWeight.Normal, FontStyle.Italic),
+        Font(R.font.ibmplexsans_light, FontWeight.Light, FontStyle.Normal),
+        Font(R.font.ibmplexsans_extralight, FontWeight.ExtraLight, FontStyle.Normal),
+        Font(R.font.ibmplexsans_extralightitalic, FontWeight.ExtraLight, FontStyle.Italic),
+        Font(R.font.ibmplexsans_lightitalic, FontWeight.Light, FontStyle.Italic),
+        Font(R.font.ibmplexsans_medium, FontWeight.Medium, FontStyle.Normal),
+        Font(R.font.ibmplexsans_mediumitalic, FontWeight.Medium, FontStyle.Italic),
+        Font(R.font.ibmplexsans_regular, FontWeight.Normal, FontStyle.Normal),
+        Font(R.font.ibmplexsans_semibold, FontWeight.SemiBold, FontStyle.Normal),
+        Font(R.font.ibmplexsans_semibolditalic, FontWeight.SemiBold, FontStyle.Italic),
+        Font(R.font.ibmplexsans_thin, FontWeight.Thin, FontStyle.Normal),
+        Font(R.font.ibmplexsans_thinitalic, FontWeight.Thin, FontStyle.Italic)
+        )
