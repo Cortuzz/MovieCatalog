0971043b19790d46b3dd2eccbba81f528f459bdd
Cortuzz <oleg.cortuzz1@gmail.com>
2022-10-21 19:29:49 +0700

    🚲feat(general): Added Main Activity
    Main Activity class with methods for changing screens

---
+++ app/src/main/java/com/example/mobiledevelopment/src/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.mobiledevelopment.src
+
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.ui.Modifier
+import com.example.mobiledevelopment.src.domain.Drawable
+import com.example.mobiledevelopment.src.login.LoginView
+import com.example.mobiledevelopment.src.main.MainView
+import com.example.mobiledevelopment.src.registration.RegistrationView
+import com.example.mobiledevelopment.ui.theme.BackgroundColor
+import com.example.mobiledevelopment.ui.theme.MobileDevelopmentTheme
+
+class MainActivity : ComponentActivity() {
+    private lateinit var currentView: Drawable
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setLoginView()
+    }
+
+    fun setLoginView() {
+        currentView = LoginView.getInstance(this)
+        draw()
+    }
+
+    fun setRegistrationView() {
+        currentView = RegistrationView.getInstance(this)
+        draw()
+    }
+
+    fun setMainView() {
+        currentView = MainView.getInstance(this)
+        draw()
+    }
+
+    private fun draw() {
+        setContent {
+            MobileDevelopmentTheme {
+                Surface(color = BackgroundColor,
+                    modifier = Modifier.fillMaxSize()) {
+                    currentView.Draw()
+                }
+            }
+        }
+    }
+}
