c38a939ae44dd45eb6897aefbb173b9d5878774f
Cortuzz <oleg.cortuzz1@gmail.com>
2022-11-01 05:31:58 +0700

    🚲feat(requests): Added object for Non-SSL connection
    BREAKING CHANGE: Do not use it in production. For debug only.

--- app/src/main/java/com/example/mobiledevelopment/include/retrofit/RetrofitClient.kt
+++ app/src/main/java/com/example/mobiledevelopment/include/retrofit/RetrofitClient.kt
@@ -1,16 +1,20 @@
 package com.example.mobiledevelopment.include.retrofit
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+
 object RetrofitClient {
     private var retrofit: Retrofit? = null
+    var okHttpClient: OkHttpClient = UnsafeOkHttpClient.unsafeOkHttpClient
 
     fun getClient(baseUrl: String): Retrofit {
         if (retrofit == null) {
             retrofit = Retrofit.Builder()
                 .baseUrl(baseUrl)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
                 .build()
         }
         return retrofit!!
