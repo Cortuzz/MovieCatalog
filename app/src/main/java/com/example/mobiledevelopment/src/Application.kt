d24135e35eef2fe4ea66f03e9e488ed05bb3b6a3
Cortuzz <oleg.cortuzz1@gmail.com>
2022-11-06 07:05:06 +0700

    🪓refactor(view, composes): Generally refactored all views
    All classes for views had been removed
    All NavHost actions now only with callbacks
    Singletone pattern had been removed

--- app/src/main/java/com/example/mobiledevelopment/src/Application.kt
+++ app/src/main/java/com/example/mobiledevelopment/src/Application.kt
@@ -4,39 +4,67 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.example.mobiledevelopment.src.login.LoginView
-import com.example.mobiledevelopment.src.main.MainView
-import com.example.mobiledevelopment.src.movie.MovieView
-import com.example.mobiledevelopment.src.registration.RegistrationView
+import androidx.navigation.compose.rememberNavController
+import com.example.mobiledevelopment.src.login.LoginScreen
+import com.example.mobiledevelopment.src.main.MainScreen
+import com.example.mobiledevelopment.src.movie.MovieScreen
+import com.example.mobiledevelopment.src.profile.ProfileScreen
+import com.example.mobiledevelopment.src.registration.RegistrationScreen
+import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
+import com.google.accompanist.navigation.material.rememberBottomSheetNavigator
 
 
+@OptIn(ExperimentalMaterialNavigationApi::class)
 @Composable
 fun SetupNavGraph(
     navController: NavHostController
 ) {
+    val bottomSheetNavigator = rememberBottomSheetNavigator()
+    val bottomNavController = rememberNavController(bottomSheetNavigator)
+
+    val navigateToLogin = { navController.navigate("login_screen") { popUpTo(0) } }
+    val navigateToRegistration = { navController.navigate("registration_screen") { popUpTo(0) } }
+    val navigateToMain = { navController.navigate("main_screen") { popUpTo(0) } }
+    val navigateToProfile = { navController.navigate("profile_screen") { popUpTo(0) } }
+    val navigateToMovie = { navController.navigate("movie_screen") }
+
     NavHost(
         navController = navController,
         startDestination = Screen.Login.route
     ) {
-        composable(
-            route = Screen.Login.route
-        ) {
-            LoginView(navController).Draw()
+        composable(route = Screen.Login.route) {
+            LoginScreen(
+                navToMain = navigateToMain,
+                navToRegistration = navigateToRegistration
+            )
         }
-        composable(
-            route = Screen.Registration.route
-        ) {
-            RegistrationView(navController).Draw()
+        composable(route = Screen.Registration.route) {
+            RegistrationScreen(
+                navToLogin = navigateToLogin,
+                navToMain = navigateToMain
+            )
         }
-        composable(
-            route = Screen.Main.route
-        ) {
-            MainView.getInstance(navController).Draw()
+        composable(route = Screen.Main.route) {
+
+            MainScreen(
+                navToLogin = navigateToLogin,
+                navToMovie = navigateToMovie,
+                navToProfile = navigateToProfile
+            )
         }
-        composable(
-            route = Screen.Movie.route
-        ) {
-            MovieView.getInstance(navController).Draw()
+        composable(route = Screen.Profile.route) {
+            ProfileScreen(
+                navToLogin = navigateToLogin,
+                navToMain = navigateToMain
+            )
+        }
+        composable(route = Screen.Movie.route) {
+            //viewModel.getMovie()
+            //viewModel.clearReview()
+            MovieScreen(
+                navToLogin = navigateToLogin,
+                navToMain = navigateToMain
+            )
         }
     }
 }
--- app/src/main/java/com/example/mobiledevelopment/src/registration/RegistrationView.kt
+++ app/src/main/java/com/example/mobiledevelopment/src/registration/RegistrationView.kt
@@ -1,5 +1,7 @@
 package com.example.mobiledevelopment.src.registration
 
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
@@ -12,6 +14,7 @@ import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
@@ -29,216 +32,144 @@ import com.example.mobiledevelopment.src.registration.domain.*
 import com.example.mobiledevelopment.ui.theme.*
 import com.example.mobiledevelopment.ui.theme.composes.*
 
-class RegistrationView(private val navController: NavHostController): Drawable {
-    private val viewModel: RegistrationViewModel = RegistrationViewModel()
+private val viewModel: RegistrationViewModel = RegistrationViewModel()
+private lateinit var navigateToMain: () -> Unit
+private lateinit var navigateToLogin: () -> Unit
 
-    @Composable
-    override fun Draw() {
-        RegistrationFailDialog()
+@Composable
+fun RegistrationScreen(navToMain: () -> Unit, navToLogin: () -> Unit) {
+    viewModel.restoreSize()
+    navigateToLogin = navToLogin
+    navigateToMain = navToMain
 
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            LoadingIndicator(
-                Modifier.requiredSize(100.dp)
-            ) { viewModel.registrationState.value == RegistrationState.Loading }
+    RegistrationContent()
+}
+
+@Composable
+fun RegistrationContent() {
+    RegistrationFailDialog()
+
+    val size by animateDpAsState(
+        targetValue = viewModel.logoSize.value,
+        animationSpec = tween(1000),
+        finishedListener = {
+            navigateToLogin()
         }
+    )
 
-        Column(
-            verticalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier
-                .padding(top = 32.dp),
-            horizontalAlignment = Alignment.CenterHorizontally) {
-            Logo(
-                Modifier.size(150.dp), Alignment.Center)
-
-            Label()
-            Fields()
-        }
-
-        Buttons()
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        LoadingIndicator(
+            Modifier.requiredSize(100.dp)
+        ) { viewModel.registrationState.value == RegistrationState.Loading }
     }
 
-    @Composable
-    fun RegistrationFailDialog() {
-        if (viewModel.registrationState.value in listOf(RegistrationState.Idle, RegistrationState.Loading))
-            return
+    Column(
+        verticalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier.padding(top = 56.dp),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Logo(Modifier.height(size), Alignment.Center)
 
-        Dialog(
-            title = "Регистрация отклонена",
-            text = when (viewModel.registrationState.value) {
-                RegistrationState.Error -> "Нет подключения к интернету или сервер недоступен"
-                RegistrationState.UserExist -> "Пользователь с таким логином уже существует"
-                else -> "Произошла внутренняя ошибка приложения, отчет об ошибке отправлен разработчикам"
-            },
-            onDismissRequest = {
-                viewModel.registrationState.value = RegistrationState.Idle
-            }
-        )
+        Label()
+        Fields()
     }
 
-    @Composable
-    fun WrongFieldText(text: String, correct: MutableState<Boolean>) {
-        if (correct.value)
-            return
+    Buttons()
 
+
+}
+
+@Composable
+fun RegistrationFailDialog() {
+    if (viewModel.registrationState.value in listOf(RegistrationState.Idle, RegistrationState.Loading))
+        return
+
+    Dialog(
+        title = "Регистрация отклонена",
+        text = when (viewModel.registrationState.value) {
+            RegistrationState.Error -> "Нет подключения к интернету или сервер недоступен"
+            RegistrationState.UserExist -> "Пользователь с таким логином уже существует"
+            else -> "Произошла внутренняя ошибка приложения, отчет об ошибке отправлен разработчикам"
+        },
+        onDismissRequest = {
+            viewModel.registrationState.value = RegistrationState.Idle
+        }
+    )
+}
+
+@Composable
+fun Label() {
+    Row(
+        modifier = Modifier.fillMaxWidth().requiredHeight(32.dp),
+        horizontalArrangement = Arrangement.Start
+    ) {
         Text(
-            modifier = Modifier.padding(horizontal = 18.dp),
-            text = text,
-            color = AccentColor,
+            text = registrationText,
             fontFamily = IBMPlex,
-            fontWeight = FontWeight.Normal,
-            fontSize = 14.sp,
-            lineHeight = 18.sp,
+            fontWeight = FontWeight.Bold,
+            fontSize = 24.sp,
+            lineHeight = 32.sp,
             letterSpacing = 0.5.sp,
+            color = AccentColor,
+            modifier = Modifier.padding(start = 16.dp)
         )
     }
+}
 
-    @Composable
-    fun Label() {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Start
-        ) {
-            Text(
-                text = registrationText,
-                fontFamily = IBMPlex,
-                fontWeight = FontWeight.Bold,
-                fontSize = 24.sp,
-                lineHeight = 32.sp,
-                letterSpacing = 0.5.sp,
-                color = AccentColor,
-                modifier = Modifier.padding(start = 16.dp)
+@Composable
+fun Fields() {
+    // TODO
+    val names = listOf(loginText, emailText, nameText, passwordText, repeatPasswordText, birthDateText)
+    val enums = listOf(ViewField.Login, ViewField.Email, ViewField.Name, ViewField.Password, ViewField.RepeatPassword, ViewField.DateOfBirth)
+    val hidden = listOf(false, false, false, true, true, false)
+    val text = listOf(
+        "Логин слишком короткий",
+        "Некорректный E-mail",
+        "Имя слишком короткое",
+        "Ненадежный пароль",
+        "Пароли не совпадают",
+        "Неверная дата рождения"
+    )
+
+    Column(modifier = Modifier
+        .padding(top = 16.dp, bottom = 132.dp)
+        .verticalScroll(rememberScrollState())) {
+        for (i in 0 until(6)) {
+            InputText(
+                label = names[i],
+                viewModel.getMutableState(enums[i]),
+                onChange = { viewModel.checkFullness() },
+                isHidden = hidden[i],
+                isDate = enums[i] == ViewField.DateOfBirth
             )
+
+            Spacer(Modifier.height(2.dp))
+            WrongFieldText(text = text[i], correct = viewModel.isFieldCorrect(enums[i]))
+            Spacer(Modifier.height(14.dp))
         }
+
+        GenderField { field, str -> viewModel.changeField(field, str) }
+        Spacer(Modifier.height(32.dp))
     }
-
-    @Composable
-    fun Fields() {
-        // TODO
-        val names = listOf(loginText, emailText, nameText, passwordText, repeatPasswordText, birthDateText)
-        val enums = listOf(ViewField.Login, ViewField.Email, ViewField.Name, ViewField.Password, ViewField.RepeatPassword, ViewField.DateOfBirth)
-        val hidden = listOf(false, false, false, true, true, false)
-        val text = listOf(
-            "Логин слишком короткий",
-            "Некорректный E-mail",
-            "Имя слишком короткое",
-            "Ненадежный пароль",
-            "Пароли не совпадают",
-            "Неверная дата рождения"
-        )
-
-        Column(modifier = Modifier
-            .padding(top = 16.dp, bottom = 132.dp)
-            .verticalScroll(rememberScrollState())) {
-            for (i in 0 until(6)) {
-                InputText(
-                    label = names[i],
-                    viewModel.getMutableState(enums[i]),
-                    onChange = { viewModel.checkFullness() },
-                    isHidden = hidden[i],
-                    isDate = enums[i] == ViewField.DateOfBirth
-                )
+}
 
 
-                Spacer(Modifier.height(2.dp))
-                WrongFieldText(text = text[i], correct = viewModel.isFieldCorrect(enums[i]))
-                Spacer(Modifier.height(16.dp))
-            }
-
-            GenderField()
-            Spacer(Modifier.height(32.dp))
-        }
-    }
-
-    @Composable
-    fun GenderField() {
-        val selectedValue = rememberSaveable { mutableStateOf("") }
-        val items = listOf(
-            GenderViewData(maleOptionText, RoundedCornerShape(8.dp, 0.dp, 0.dp, 8.dp), 0.5f, 1.dp),
-            GenderViewData(femaleOptionText, RoundedCornerShape(0.dp, 8.dp, 8.dp, 0.dp), 1f, 0.dp)
-        )
-
-        val isSelectedItem: (String) -> Boolean = { selectedValue.value == it }
-        val onChangeState: (GenderViewData) -> Unit = {
-            selectedValue.value = it.name
-            viewModel.changeField(ViewField.Gender, items.indexOf(it).toString())
-        }
-
-        Row(
-            horizontalArrangement = Arrangement.Start,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 15.dp, end = 16.dp)
-        ) {
-
-
-            items.forEach { item ->
-                OutlinedButton(
-                    onClick = { onChangeState(item) },
-                    modifier = Modifier
-                        .fillMaxWidth(item.fraction)
-                        .offset(x = item.offset)
-                        .selectable(
-                            selected = isSelectedItem(item.name),
-                            onClick = { onChangeState(item) },
-                            role = Role.RadioButton
-                        ),
-
-                    enabled = !isSelectedItem(item.name),
-                    colors = getRadioButtonColor(),
-                    border = BorderStroke(1.dp, OutlineColor),
-                    shape = item.shape
-                ) {
-                    Text(
-                        text = item.name,
-                        fontFamily = IBMPlex,
-                        fontWeight = FontWeight(400),
-                        fontSize = 14.sp,
-                        lineHeight = 18.sp,
-                        letterSpacing = 0.5.sp,
-                        modifier = Modifier.padding(top = 1.dp, bottom = 5.dp)
-                    )
-                }
-            }
-        }
-
-    }
-    
-    @Composable
-    private fun getRadioButtonColor(): ButtonColors {
-        return ButtonDefaults.buttonColors(
-            contentColor = TextColor,
-            backgroundColor = Color.Transparent,
-            disabledContentColor = SelectedTextColor,
-            disabledBackgroundColor = AccentColor
-        )
-    }
-
-    @Composable
-    fun Buttons() {
-        Column(
-            verticalArrangement = Arrangement.Bottom,
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier
-                .padding(bottom = 16.dp)
-                .fillMaxHeight()
-        ) {
-            PrimaryButton(name = registerText, action = {
-                viewModel.handleRegistrationClick {
-                    navController.navigate("main_screen") {
-                        popUpTo(0)
-                    }
-                } },
-                isEnabled = viewModel.fullness)
-            Spacer(Modifier.height(8.dp))
-            SecondaryButton(name = goToLoginText, action = {
-                navController.navigate("login_screen") {
-                    popUpTo(0)
-                }
-            })
-        }
+@Composable
+fun Buttons() {
+    Column(
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .padding(bottom = 16.dp)
+            .fillMaxHeight()
+    ) {
+        PrimaryButton(name = registerText, action = {
+            viewModel.handleRegistrationClick { navigateToMain() } },
+            isEnabled = viewModel.fullness)
+        Spacer(Modifier.height(8.dp))
+        SecondaryButton(name = goToLoginText, action = { viewModel.handleLoginScreenClick() })
     }
 }
