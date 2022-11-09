0035dc1ff8dfc93e5ca121fe40c8b47c97c23478
Cortuzz <oleg.cortuzz1@gmail.com>
2022-11-09 18:54:26 +0700

    🚨fix(splash): Hotfixed crash bug

--- app/src/main/java/com/example/mobiledevelopment/SplashScreen.kt
+++ app/src/main/java/com/example/mobiledevelopment/SplashScreen.kt
@@ -27,12 +27,12 @@ import kotlinx.coroutines.delay
 @SuppressLint("CustomSplashScreen")
 class SplashScreen: ComponentActivity() {
     private val authManager = TokenProviderService.getInstance(this)
-    private lateinit var splashScreen: androidx.core.splashscreen.SplashScreen
+    private var splashScreen: androidx.core.splashscreen.SplashScreen? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S){
             splashScreen = installSplashScreen()
-            splashScreen.setKeepOnScreenCondition { true }
+            splashScreen?.setKeepOnScreenCondition { true }
         }
         super.onCreate(savedInstanceState)
 
@@ -60,7 +60,7 @@ class SplashScreen: ComponentActivity() {
                 goToApplication()
             },
             onFailureAction = {
-                splashScreen.setKeepOnScreenCondition { false }
+                splashScreen?.setKeepOnScreenCondition { false }
                 alert()
             }
         )
